6c05377ee70a054a2e58a0ba0623cb5f30ec227e
cuhrmunken <elliott.davis@crestonetech.com>
2024-08-17 20:24:48 -0700

    adds API call to get current conditions

--- assets/script.js
+++ assets/script.js
@@ -2,18 +2,32 @@
 const searchCityButtonEl = document.getElementById('search-city-button');
 const searchCityEl = document.getElementById('search-city');
 
+// DATA
+const apiKey = "1ac8e0e868989abf2abc571cd717e4b3";
+
 async function handleCitySearch() {
-    console.log(await getCoordinates(searchCityEl.value));
+    const coordinates = await getCoordinates(searchCityEl.value);
+    console.log(await getCurrentWeather(coordinates));
 }
 
 async function getCoordinates(cityName) {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=1ac8e0e868989abf2abc571cd717e4b3&appid`);
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${apiKey}`);
     const theData = await response.json()
     const lat = theData[0].lat;
     const lon = theData[0].lon;
-    // console.log("lat", lat, "lon", lon);
-    // console.log(theData[0])
-    return { lat: lat, lon: lon };
+
+    return { lat, lon };
+}
+
+async function getCurrentWeather(coordinates) {
+    const { lat, lon } = coordinates;
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
+    const currentWeather = await response.json();
+    const windSpeed = currentWeather.wind.speed;
+    const { temp, humidity } = currentWeather.main;
+    const cloudPercentage = currentWeather.clouds.all;
+
+    return { windSpeed, humidity, temp, cloudPercentage };
 }
 
 searchCityButtonEl.addEventListener('click', handleCitySearch);
